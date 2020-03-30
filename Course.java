23f08b130ba2345d25400370834b31eca71aeef6
BojanR224 <bojan.robev@gmail.com>
2020-03-30 17:54:25 +0200

    Course class added

---
+++ Course.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class Course{
+    List<Student> students;
+
+    public Course(List<Student> students) {
+        this.students = students;
+    }
+    public void addStudent(Student s){
+        students.add(s);
+    }
+    public void deleteStudent(Student s){
+        students.remove(s);
+    }
+    public long getNumberOfSinguters(){
+        return students.stream().filter(Student::hasSignature).count();
+    }
+}
